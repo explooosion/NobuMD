c2636a87ed18cb834f9493db384ae0167d4abbba
Robby <ta7382@gmail.com>
2017-08-14 06:10:57 +0800

    update bootstrap v4 beta

--- nobumd-f/src/js/template-fix.js
+++ nobumd-f/src/js/template-fix.js
@@ -2,25 +2,25 @@
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a[href*="#"]:not([href="#"]):not(.nav-link)').click(function () {
-    return;
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 48)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+  // $('a[href*="#"]:not([href="#"]):not(.nav-link)').click(function () {
+  //   return;
+  //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  //     var target = $(this.hash);
+  //     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  //     if (target.length) {
+  //       $('html, body').animate({
+  //         scrollTop: (target.offset().top - 48)
+  //       }, 1000, "easeInOutExpo");
+  //       return false;
+  //     }
+  //   }
+  // });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
-  });
+  // $('body').scrollspy({
+  //   target: '#mainNav',
+  //   offset: 54
+  // });
 
   // Closes responsive menu when a link is clicked
   // $('.navbar-collapse>ul>li>a').click(function() {
@@ -28,13 +28,13 @@
   // });
 
   // Collapse the navbar when page is scrolled
-  $(window).scroll(function () {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  });
+  // $(window).scroll(function () {
+  //   if ($("#mainNav").offset().top > 100) {
+  //     $("#mainNav").addClass("navbar-shrink");
+  //   } else {
+  //     $("#mainNav").removeClass("navbar-shrink");
+  //   }
+  // });
 
   // Floating label headings for the contact form
   // $(function() {
