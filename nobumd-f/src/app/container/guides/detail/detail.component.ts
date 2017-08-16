f0ef4dd3fafa960f7c0c9640c7ff2ba92a0b897d
Robby <ta7382@gmail.com>
2017-08-16 15:18:23 +0800

    add tipTemplate

--- nobumd-f/src/app/container/guides/detail/detail.component.ts
+++ nobumd-f/src/app/container/guides/detail/detail.component.ts
@@ -27,22 +27,44 @@ export class DetailComponent implements OnInit {
   public pieChartData: number[] = [3, 5, 7, 2, 4, 8];
   public pieChartType: string = 'pie';
 
-  // public someRange = 5;
-  // public someRange2config: any = {
-  //   connect: true,
-  //   margin: 1,
-  //   range: {
-  //     min: 1,
-  //     max: 20
-  //   },
-  //   pips: {
-  //     mode: 'steps',
-  //     density: 10
-  //   }
-  // };
+
+  // public tipskillTemplate =
+  // `<div class='tip'>
+  // <strong class='tip-name'>天下之勢</strong>
+  // <span class='tip-content'>傷害減少10%，並將減少的傷害轉為補充自身雙倍的法力。</span>
+  // </div>`;
+
+  public tipsArr = {
+    'a01-1': `<div class='tip'>
+  <strong class='tip-name'>天下之勢</strong>
+  <span class='tip-content'>傷害減少10%，並將減少的傷害轉為補充自身雙倍的法力。</span>
+  </div>`,
+    'a01-2': `<div class='tip'>
+  <strong class='tip-name'>東照權現</strong>
+  <span class='tip-content'>恢復周圍友軍<em class='tip-step'>(75/120/165/210)</em>的生命值 ，並使他們增加35% 攻速，持續4秒。</span>
+  </em>`,
+    'a01-3': `<div class='tip'>
+  <strong class='tip-name'>天羅地網</strong>
+  <span class='tip-content'>使目標敵軍被陷阱糾纏，使其受困而無法移動<em class='tip-step'>(0.9/1.2/1.5/1.8)</em>秒並且在18方位創造出網子進行攻擊，每發網子造成<em class='tip-step'>(11/18/25/32)</em>傷害。</span>
+  </div>`,
+    'a01-4': `<div class='tip'>
+  <strong class='tip-name'>雷霆刃</strong>
+  <span class='tip-content'>召喚雷霆的威力，對周圍的敵人造成<em class='tip-step'>(75/150/225/300)</em>魔法傷害，並且在接下來5秒，攻擊時有50% 的機會，對擊中目標周圍310內的所有敵軍，額外造成<em class='tip-step'>(80/110/140/170)</em>點對魔免減半的真實傷害。</span>
+  </div>`,
+    'a01-5': `<div class='tip'>
+  <strong class='tip-name'>三河魂</strong>
+  <span class='tip-content'>施放自身的王者之氣，能增加周圍1500友軍<em class='tip-step'>(15/25/35)</em>的防禦，減少<em class='tip-step'>(15/20/25)</em>% 物理傷害與加快生命回復速率<em class='tip-step'>(5/6/7)</em>% ，持續7秒。</span>
+  </div>`
+  };
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public tipTemplate(id) {
+    return this.tipsArr[id];
   }
 
   // events
