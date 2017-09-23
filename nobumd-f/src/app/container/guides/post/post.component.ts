5bdd91d381dda343f4c241e752e7b42d8ccb2f61
Robby <ta7382@gmail.com>
2017-09-23 19:59:49 +0800

    add post - 英雄評價 & 影片分享

--- nobumd-f/src/app/container/guides/post/post.component.ts
+++ nobumd-f/src/app/container/guides/post/post.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
+  // Doughnut
+  public doughnutChartLabels: string[] = ['主力', '肉盾', '控場', '亂場', '輔助', '遠距'];
+  public doughnutChartData: Number[] = [8, 5, 3, 6, 7, 2];
+  public doughnutChartType: String = 'doughnut';
+
+  public doughnutChartOption: any = {
+    legend: {
+      labels: {
+        fontColor: '#ffcd03',
+        fontSize: 14,
+        fontFamily: 'Noto Sans TC',
+      }
+    }
+  };
+
+  // Pie
+  public pieChartLabels: string[] = ['Data1', 'Data2', 'Data3', 'Data4', 'Data5', 'Data6'];
+  public pieChartData: number[] = [3, 5, 7, 2, 4, 8];
+  public pieChartType: String = 'pie';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeChar(value: number, index: number) {
+    const drr = this.doughnutChartData;
+    if (value > 10) {
+      value = 10;
+    } else if (value < 1) {
+      value = 1;
+    }
+    drr[index] = value;
+
+    this.doughnutChartData = [drr[0], drr[1], drr[2], drr[3], drr[4], drr[5]];
+  }
 }
