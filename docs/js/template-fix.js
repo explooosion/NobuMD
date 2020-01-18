123fd2c2fe853019b0b7434aa09c09fe24b61c27
explooosion <ta7382@gmail.com>
2020-01-19 02:47:10 +0800

    Build to github host

---
+++ docs/js/template-fix.js
@@ -0,0 +1,46 @@
+(function ($) {
+  // "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a[href*="#"]:not([href="#"]):not(.nav-link)').click(function () {
+
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    target = target.offset().top;
+    $('html, body').animate({
+      scrollTop: (target - 48)
+    }, 1000, "easeInOutExpo");
+
+    return false;
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+
+  // Closes responsive menu when a link is clicked
+  $('.navbar-collapse>ul>li>a').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+      $('.scroll-top').fadeIn(300);
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+      $('.scroll-top').fadeOut(300);
+    }
+  });
+
+  // Page Top
+  $('.scroll-top').click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000, "easeInOutExpo");
+  });
+
+})(jQuery); // End of use strict
